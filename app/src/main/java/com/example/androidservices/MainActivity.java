2ba60908936d4bd4df7b38a04da89f023074ccc8
shakiz <activities335@gmail.com>
2020-11-23 00:27:08 +0600

    service with IBinder

--- app/src/main/java/com/example/androidservices/MainActivity.java
+++ app/src/main/java/com/example/androidservices/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.androidservices;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.androidservices.services.MyService;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startServiceBtn, stopServicebtn;
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //region init UI and perform UI interactions
+        initUI();
+        bindUiWithComponents();
+        //endregion
     }
+
+    //region init UI
+    private void initUI() {
+        startServiceBtn = findViewById(R.id.startService);
+        stopServicebtn = findViewById(R.id.stopService);
+        serviceIntent = new Intent(MainActivity.this, MyService.class);
+    }
+    //endregion
+
+    //region perform UI interactions
+    private void bindUiWithComponents() {
+        startServiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(serviceIntent);
+            }
+        });
+
+        stopServicebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(serviceIntent);
+            }
+        });
+    }
+    //endregion
 }
---
+++ app/src/main/java/com/example/androidservices/services/MyService.java
@@ -0,0 +1,79 @@
+package com.example.androidservices.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+import androidx.annotation.Nullable;
+import java.util.Random;
+
+public class MyService extends Service {
+    private int randomNumber;
+    private boolean enableRandomGenerator = false;
+
+    private class MyServiceBinder extends Binder{
+        public MyService getService(){
+            return MyService.this;
+        }
+    }
+
+    //This ensures that we can receive the on bind request from an external resources or agent
+    private IBinder myIBinder = new MyServiceBinder();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.i("Shakil","onBind");
+        return myIBinder;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Log.i("Shakil","onRebind");
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i("Shakil","onUnbind");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("Shakil","onStartCommand");
+        Log.i("Shakil","Thread ID:"+Thread.currentThread().getId());
+        enableRandomGenerator = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startRandomNumberGenerator();
+            }
+        }).start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRandomNumberGenerator();
+        Log.i("Shakil","onDestroy Called");
+    }
+
+    private void startRandomNumberGenerator() {
+        while (enableRandomGenerator){
+            try{
+                randomNumber = new Random().nextInt(100)+11;
+                Log.i("Shakil","Random Number : "+ randomNumber + " Thread ID:" + Thread.currentThread().getId());
+            }
+            catch (Exception e){
+                Log.i("Shakil","startRandomNumberGenerator exception: "+e.getMessage());
+            }
+        }
+    }
+
+    private void stopRandomNumberGenerator(){
+        enableRandomGenerator = false;
+    }
+}
